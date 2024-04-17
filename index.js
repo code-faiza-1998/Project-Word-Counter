3ccdd6b716d339f72eb6668e63ad81af34387e81
code-faiza-1998 <faiza5198@gmail.com>
2024-04-17 01:18:29 -0700

    CLI based Word Counter by using typescript, node.js, inquirer and chalk.

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+console.log(chalk.bgCyanBright("\t\tWELCOME TO MY WORD COUNTER\t\n\n"));
+// declare a constant 'answers' and assign it to the result of inquirer.prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "sentence",
+        type: "input",
+        message: (chalk.green("Enter your sentence to count the words : \n"))
+    }
+]);
+const words = answers.sentence.trim().split(" ");
+// print the array of word to the console
+console.log(words);
+// print the word count of the sentence to the console
+console.log(chalk.blue(`Your Sentence Word count is ${words.length}`));
